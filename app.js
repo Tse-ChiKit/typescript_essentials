dbd6d9fe7b6f25ba13be472142d777e779c809ec
Eric Xie <eric.xie@amway.com>
2021-11-24 10:26:28 +0800

    feat: add app entry file

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('ts is running!!');
+//Object Type Definition
+var person = {
+    name: 'eric',
+    age: 12,
+    role: [2, 'author']
+};
+person.role.push('admin');
+person.role.push(1231);
+console.log('ts is running!!', person);
--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('ts is running!!');
+//Object Type Definition
+
+const person: {
+  name: string;
+  age: number;
+  role: [number, string];
+} = {
+  name: 'eric',
+  age: 12,
+  role: [2, 'author'],
+};
+
+person.role.push('admin');
+
+console.log('ts is running!!', person);
