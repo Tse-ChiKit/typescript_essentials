c01d7518aea653f2c74af1e0a2ed576082d750da
Eric Xie <eric.xie@amway.com>
2021-11-24 11:16:42 +0800

    feat: add object, tuple, array and union types

--- app.ts
+++ app.ts
@@ -1,15 +1,34 @@
-//Object Type Definition
+//Enums
+enum Gender {
+  Male = 'M',
+  Female = 'F',
+}
 
+//Object Type Definition
 const person: {
   name: string;
   age: number;
-  role: [number, string];
+  role: [number, string]; // Tuple
+  phones: string[];
+  gender: Gender;
 } = {
   name: 'eric',
   age: 12,
   role: [2, 'author'],
+  phones: ['apple 12', 'huawei P40'],
+  gender: Gender.Male,
 };
 
+//tuple allows push and pop
 person.role.push('admin');
+
+//union type
+function handleAdd(input1: number | string, input2: number | string) {
+  if (typeof input1 == 'number' && typeof input2 == 'string') {
+    return input1 + input2;
+  } else return 'please input 2 numbers';
+}
+
+console.log(handleAdd(1, 2));
 
 console.log('ts is running!!', person);
